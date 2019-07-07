9c564788ef9f1ac30ad5d822ef2127bd74aa4669
gujjav <gcvikranth@yahoo.com>
2019-07-07 14:01:35 +0530

    Changing service API URL

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const INSTRUCTOR = 'testuser'
-const COURSE_API_URL = 'http://localhost:8080'
+const COURSE_API_URL = 'http://35.240.171.195:8080'
 const INSTRUCTOR_API_URL = `${COURSE_API_URL}/instructors/${INSTRUCTOR}`
 
 class DataService {
